38fb3f186723827687b26b85fb0fc0911dec8ed5
Joel Santos Souza <joesanto@c2r6s6.42porto.com>
2025-12-15 11:39:21 +0000

    I must understand what exactly is the if __name__ == __main__:

---
+++ module01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if  __name__ == "__main__":
+    name: str = "cactus"
+    height: str = "18m"
+    age: str = "100"
+    print("=== Welcome to my Garden ===")
+    print("Plant: " + name)
+    print("Height: " + height)
+    print("Age: " + age)
+    print("\n=== End of Program ===")
