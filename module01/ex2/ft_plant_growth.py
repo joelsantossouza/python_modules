fa9b7a0f8467d3361f3935b697fdbc4148ab3d5c
Joel Santos Souza <joesanto@c2r6s6.42porto.com>
2025-12-15 12:53:38 +0000

    Could learn more about methods

---
+++ module01/ex2/ft_plant_growth.py
@@ -0,0 +1,26 @@
+class   Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name: str = name
+        self.height: int = height
+        self.age: int = age
+    def growth(self, size: int) -> None:
+        self.height += size
+    def aging(self, time: int) -> None:
+        self.age += time
+    def get_info(self) -> None:
+        print(f"{self.name} : {self.height}cm, {self.age} days old")
+
+if __name__ == "__main__":
+    rose: Plant = Plant("Rose", 25, 30)
+    weeks: int = 1
+    day: int = 0
+    print(f"=== Day {day} ===")
+    rose.get_info()
+    while (weeks > 0):
+        rose.growth(6)
+        rose.aging(7)
+        day += 7
+        weeks -= 1
+        print(f"=== Day {day} ===")
+        rose.get_info()
+        print("Growth this week: +6cm")
