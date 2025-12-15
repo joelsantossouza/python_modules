7579315cffe0d5dfee15aa78a7c5a1a2f2d31cd2
joel <joelsantossouza08@gmail.com>
2025-12-15 22:52:18 +0000

    Could learn about inheritance of classes. I could understand its importance: So we can have general family of objects like character, but we want for exemple a Warrior, the will have the base structure of a character + its own specialties

--- module01/ex5/ft_plant_types.py
+++ module01/ex5/ft_plant_types.py
@@ -5,12 +5,12 @@ class Plant:
         self.age = age
 
     def get_info(self, end: str = "\n") -> None:
-        print(f"{self.name}: {self.height}cm, {self.days} days", end=end)
+        print(f"{self.name}: {self.height}cm, {self.age} days", end=end)
 
 
 class Flower(Plant):
     def __init__(self, name: str, height: int, age: int, color: str) -> None:
-        super().__init__(self, name, height, age)
+        super().__init__(name, height, age)
         self.color = color
 
     def bloom(self) -> None:
@@ -18,7 +18,7 @@ class Flower(Plant):
 
     def get_info(self) -> None:
         print("[Flower] - ", end="")
-        super().get_info(self, end="")
+        super().get_info(end="")
         print(f", {self.color} color")
 
 
@@ -29,11 +29,11 @@ class Tree(Plant):
         self.trunk_diameter = trunk_diameter
 
     def produce_shade(self) -> None:
-        print(f"{self.name} provides {self.height * self.trunk_diameter} square meters of shade")
+        print(f"{self.name} provides {(self.height * self.trunk_diameter) / 100} square meters of shade")
 
     def get_info(self) -> None:
         print("[Tree] - ", end="")
-        super().get_info(self, end="")
+        super().get_info(end="")
         print(f", {self.trunk_diameter}cm diameter")
 
 
@@ -41,27 +41,47 @@ class Vegetable(Plant):
     def __init__(self, name: str, height: int, age: int, harvest_season: str, nutritional_value: str) -> None:
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
-        self.nutricional_value = nutricional_value
+        self.nutritional_value = nutritional_value
 
     def nutri_info(self) -> None:
-        print(f"{self.name} is rich in vitamin {self.nutricional_value}")
+        print(f"{self.name} is rich in vitamin {self.nutritional_value}")
 
     def get_info(self) -> None:
         print("[Vegetable] - ", end="")
-        super().get_info(self, end="")
+        super().get_info(end="")
         print(f", {self.harvest_season} harvest")
 
 
 if __name__ == "__main__":
-    print("=== Garden Plant Types ===\n")
+    print("=== Garden Plant Types ===")
+    print("\n-- Flowers --")
     rose: Flower = Flower("Rose", 25, 30, "red")
     rose.get_info()
     rose.bloom()
+    print("")
 
+    tulips: Flower = Flower("Tulips", 15, 7, "yellow")
+    tulips.get_info()
+    tulips.bloom()
+    print("")
+
+    print("\n-- Trees --")
     oak: Tree = Tree("Oak", 500, 1825, 50)
     oak.get_info()
     oak.produce_shade()
+    print("")
 
-    tomato: Vegetable = Vegetable("Tomato", 80, 90, "summer")
+    pines: Tree = Tree("Pines", 2500, 36500, 80)
+    pines.get_info()
+    pines.produce_shade()
+    print("")
+
+    print("\n-- Vegetables --")
+    tomato: Vegetable = Vegetable("Tomato", 80, 90, "summer", "C")
     tomato.get_info()
     tomato.nutri_info()
+    print("")
+
+    spinach: Vegetable = Vegetable("Spinach", 20, 80, "spring", "K")
+    spinach.get_info()
+    spinach.nutri_info()
