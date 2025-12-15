d11dc7c4db89a5795394af3810802cfc9039be7f
Joel Santos Souza <joesanto@c2r6s6.42porto.com>
2025-12-15 14:31:14 +0000

    Understand how to do the recursion, and could learn the defaults parameters values in python

--- module00/ex5/ft_count_harvest_recursive.py
+++ module00/ex5/ft_count_harvest_recursive.py
@@ -1,2 +1,8 @@
-def ft_count_harvest_recursive() -> None
-
+def ft_count_harvest_recursive(start: int = 1, max_days: int = 0) -> None:
+    if start == 1:
+        max_days = int(input("Days until harvest: "))
+    if start > max_days:
+        print("Harvest time!")
+        return
+    print(f"Day {start}")
+    ft_count_harvest_recursive(start + 1, max_days)
