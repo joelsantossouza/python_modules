787394dd1ad02b29598cff7275221ff02f46e199
Joel Santos Souza <joesanto@c2r1s6.42porto.com>
2025-12-17 06:31:20 +0000

    Learning about lists and operations on it

---
+++ module03/ex1/ft_score_analytics.py
@@ -0,0 +1,27 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Player Score Analytics ===")
+    argc: int = len(sys.argv) - 1
+    if argc < 2:
+        print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+    else:
+        scores: list[int] = [None] * argc
+        i: int = 0
+        try:
+            while (i < argc):
+                scores[i] = int(sys.argv[i + 1])
+                i += 1
+        except Exception as e:
+            print(f"[ERROR] - {e}")
+        else:
+            total_score: int = sum(scores)
+            min_score: int = min(scores)
+            max_score: int = max(scores)
+            print(f"Scores processed: {scores}")
+            print(f"Total players: {argc}")
+            print(f"Total score: {total_score}")
+            print(f"Average score: {total_score / argc}")
+            print(f"High score: {max_score}")
+            print(f"Low score: {min_score}")
+            print(f"Score range: {max_score - min_score}")
