2db462cd5422d6cf6a0dc9094a4eab0680018af6
joel <joelsantossouza08@gmail.com>
2025-12-21 12:45:30 +0000

    Doing the filter data method

--- module05/ex1/data_stream.py
+++ module05/ex1/data_stream.py
@@ -11,6 +11,23 @@ class DataStream(ABC):
     def process_batch(self, data_batch: List[Any]) -> str:
         ...
 
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        if criteria != "critical":
+            return data_batch
+        try:
+            filtered: List[Any] = [
+                data for data in data_batch
+                if (data[:5] == "temp:" and float(data[5:]) < 25) or
+                (data[:9] == "humidity:" and int(data[9:]) < 50) or
+                (data[:9] == "pressure:" and int(data[9:]) < 1000) or
+                (data[:4] == "buy:" and int(data[4:]) < 125) or
+                (data[:5] == "sell:" and int(data[5:]) < 125)
+            ]
+        except Exception:
+            print("ERROR: Invalid data")
+            return data_batch
+
 
 class SensorStream(DataStream):
     """Sensor to track temperature, humidity and pressure"""
@@ -20,11 +37,15 @@ class SensorStream(DataStream):
         total_temp: float = 0
         ntemps: int = 0
         for data in data_batch:
-            readings += 1
             try:
                 if data[:5] == "temp:":
                     total_temp += float(data[5:])
                     ntemps += 1
+                    readings += 1
+                elif data[:9] == "humidity:":
+                    readings += 1
+                elif data[:9] == "pressure:":
+                    readings += 1
             except Exception:
                 print(
                     f"ERROR: '{data}' is an invalid value, options:\n"
@@ -47,12 +68,13 @@ class TransactionStream(DataStream):
         operations: int = 0
         profit: int = 0
         for transaction in data_batch:
-            operations += 1
             try:
                 if transaction[:4] == "buy:":
                     profit += int(transaction[4:])
+                    operations += 1
                 elif transaction[:5] == "sell:":
                     profit -= int(transaction[5:])
+                    operations += 1
             except Exception:
                 print(
                     f"ERROR: '{transaction}' is an invalid value, options:\n"
@@ -60,7 +82,7 @@ class TransactionStream(DataStream):
                     " - sell=value\n"
                 )
         return f"Transaction analysis: {operations} operations, " \
-                f"new flow: {profit:+d} units"
+                f"net flow: {profit:+d} units"
 
 
 class EventStream(DataStream):
@@ -73,11 +95,33 @@ class EventStream(DataStream):
             try:
                 if event == "error":
                     errors += 1
-                events += 1
+                    events += 1
+                elif event == "login":
+                    events += 1
+                elif event == "logout":
+                    events += 1
             except Exception:
                 print(f"ERROR: '{event}' is an invalid value")
         return f"Event analysis: {events} events, " \
                 f"{errors} error detected"
+
+
+class StreamProcessor(DataStream):
+    """Process all type of stream (Sensor, Transaction and Event)"""
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        sensor: SensorStream = SensorStream("SENSOR_002")
+        sensor_str: str = sensor.process_batch(data_batch)
+
+        transaction: TransactionStream = TransactionStream("TRANS_002")
+        transaction_str: str = transaction.process_batch(data_batch)
+
+        events: EventStream = EventStream("EVENT_002")
+        events_str: str = events.process_batch(data_batch)
+
+        return f" - {sensor_str}\n" \
+                f" - {transaction_str}\n" \
+                f" - {events_str}\n"
 
 
 if __name__ == "__main__":
@@ -106,3 +150,15 @@ if __name__ == "__main__":
 
     print("\n=== Polymorphic Stream Processing ===")
     print("Processing mixed stream types through unified interface...")
+    stream: StreamProcessor = StreamProcessor("STREAM_001")
+    input: List[str] = [
+        "buy:500", "buy:15", "pressure:100", "error", "temp:-10",
+        "login", "sell:500", "logout", "sell:10"
+    ]
+
+    print("\nBatch 1 Results:")
+    print(stream.process_batch(input))
+
+    print("\nStream filtering active: High-priority data only")
+    print(input)
+    print(stream.filter_data(input, "critical"))
