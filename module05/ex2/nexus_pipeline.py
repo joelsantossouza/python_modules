0f617275181d3602a2c4bc2688d08975fb66e618
Joel Santos Souza <joesanto@c2r2s6.42porto.com>
2025-12-22 17:33:06 +0000

    Doing the last one

--- module05/ex2/nexus_pipeline.py
+++ module05/ex2/nexus_pipeline.py
@@ -12,7 +12,7 @@ class ProcessingStage(Protocol):
 class InputStage:
     """Input validation and parsing"""
 
-    def process(data: Any) -> Dict:
+    def process(self, data: Any) -> Dict:
         valid: bool = True
         if data is None:
             valid = False
@@ -25,7 +25,7 @@ class InputStage:
 class TransformStage:
     """Data transformation and enrichment"""
 
-    def process(data: Any) -> Dict:
+    def process(self, data: Any) -> Dict:
         return {
             "metadata": "Processed",
             "data": data
@@ -35,7 +35,7 @@ class TransformStage:
 class OutputStage:
     """Output formatting and delivery"""
 
-    def process(data: Any) -> str:
+    def process(self, data: Any) -> str:
         return f"Output: {data}"
 
 
@@ -64,26 +64,75 @@ class JSONAdapter(ProcessingPipeline):
         if not isinstance(data, Dict):
             print("ERROR: You must pass a valid JSON format")
             return None
+        sensor: str = "unknown"
+        feedback: str = ""
         try:
-            if not isinstance(data["value"], int) or not isinstance(data["unit"], str):
+            if not isinstance(data["sensor"], str) or not isinstance(data["value"], int) or not isinstance(data["unit"], str):
                 raise Exception
             if data["sensor"] == "temp":
                 sensor: str = "temperature"
-                feedback: str = "Normal range" if 2 < data["value"] < 30 else "Critical range"
+                feedback: str = "(Normal range)" if 2 < data["value"] < 30 else "(Critical range)"
             elif data["sensor"] == "humidity":
                 sensor: str = "humidity"
-                feedback: str = "Normal range" if 20 < data["value"] < 70 else "Critical range"
+                feedback: str = "(Normal range)" if 20 < data["value"] < 70 else "(Critical range)"
             elif data["sensor"] == "pressure":
                 sensor: str = "pressure"
-                feedback: str = "Normal range" if 100 < data["value"] < 1000 else "Critical range"
-            else:
-                sensor: str = "unknown"
+                feedback: str = "(Normal range)" if 100 < data["value"] < 1000 else "(Critical range)"
         except Exception:
             print("ERROR: You must provide a sensor type, its value and the measure unit")
-        parsed: str = f"{sensor} reading: {data['value']}{data['unit']} ({feedback})"
+            return None
+        parsed: str = f"{sensor} reading: {data['value']}{data['unit']} {feedback}"
+        for stage in self.stages:
+            result: Any = stage.process(parsed)
+            if isinstance(result, Dict):
+                try:
+                    if not result["valid"]:
+                        print("ERROR: Invalid processing...")
+                        return None
+                except Exception:
+                    parsed = f"{result['metadata']} {parsed}"
+            else:
+                parsed = result
+        return parsed
+
+
+class CSVAdapter(ProcessingPipeline):
+    """Process specific input of type CSV"""
+
+    def __init__(self, pipeline_id: int) -> None:
+        if isinstance(pipeline_id, int):
+            self.id = pipeline_id
+        else:
+            print("ERROR: A pipeline ID must be integer")
+
+    def process(self, data: Any) -> Union[str, Any]:
+        if not isinstance(data, str):
+            print("ERROR: You must pass a valid CSV format")
+            return None
+        csv: List[str] = data.split("\n")
+        actions: int = 0
+        for row in csv:
+            if row != "":
+                actions += 1
+        parsed: str = f"user activity logged: {actions} actions"
+        for stage in self.stages:
+            result: Any = stage.process(parsed)
+            if isinstance(result, Dict):
+                try:
+                    if not result["valid"]:
+                        print("ERROR: Invalid processing...")
+                        return None
+                except Exception:
+                    parsed = f"{result['metadata']} {parsed}"
+            else:
+                parsed = result
+        return parsed
 
 
 if __name__ == "__main__":
     print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===")
 
     print("\nInitializing Nexus Manager...")
+    test: CSVAdapter = CSVAdapter(100) 
+    result: str = test.process("\n\n\n\n")
+    print(result)
