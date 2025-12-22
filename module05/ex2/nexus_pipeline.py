f462f051ca8b7616f794c1f340b4d2ef22d97f2f
Joel Santos Souza <joesanto@c2r2s6.42porto.com>
2025-12-22 15:32:36 +0000

    Understanding polymorphism

--- module05/ex2/nexus_pipeline.py
+++ module05/ex2/nexus_pipeline.py
@@ -55,6 +55,32 @@ class JSONAdapter(ProcessingPipeline):
     """Process specific input of type JSON"""
 
     def __init__(self, pipeline_id: int) -> None:
+        if isinstance(pipeline_id, int):
+            self.id = pipeline_id
+        else:
+            print("ERROR: A pipeline ID must be integer")
+
+    def process(self, data: Any) -> Union[str, Any]:
+        if not isinstance(data, Dict):
+            print("ERROR: You must pass a valid JSON format")
+            return None
+        try:
+            if not isinstance(data["value"], int) or not isinstance(data["unit"], str):
+                raise Exception
+            if data["sensor"] == "temp":
+                sensor: str = "temperature"
+                feedback: str = "Normal range" if 2 < data["value"] < 30 else "Critical range"
+            elif data["sensor"] == "humidity":
+                sensor: str = "humidity"
+                feedback: str = "Normal range" if 20 < data["value"] < 70 else "Critical range"
+            elif data["sensor"] == "pressure":
+                sensor: str = "pressure"
+                feedback: str = "Normal range" if 100 < data["value"] < 1000 else "Critical range"
+            else:
+                sensor: str = "unknown"
+        except Exception:
+            print("ERROR: You must provide a sensor type, its value and the measure unit")
+        parsed: str = f"{sensor} reading: {data['value']}{data['unit']} ({feedback})"
 
 
 if __name__ == "__main__":
