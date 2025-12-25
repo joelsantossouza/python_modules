bf8efb3e1940ab909dc095c6f796a23f4ce07874
joel <joelsantossouza08@gmail.com>
2025-12-25 14:49:08 +0000

    Starting create new cards type

--- module07/ex1/SpellCard.py
+++ module07/ex1/SpellCard.py
@@ -8,20 +8,34 @@ class SpellCard(Card):
                 file=stderr
             )
             exit(1)
+        super().__init__(name, cost, rarity)
+        self._info |= {
+            "type": "Spell",
+            "effect": effect_type,
+        }
 
     def validate(self, name: str, cost: int, rarity: str, effect_type: str) -> bool:
         if not super().validate(name, cost, rarity):
             return False
-        effects_type = list[str] = [
-            "damage", "heal", "buff", "debuff"
-        ]
-        if effect_type not in effects_type:
+        if effect_type == "damage":
+            effect_type = "Deal 3 damage to target"
+        elif effect_type == "heal":
+            effect_type = "Restore 3 health points"
+        elif effect_type == "buff":
+            effect_type = "Grant +2 attack points"
+        elif effect_type == "unbuff":
+            effect_type = "Reduce target attack by 2"
+        else:
             return False
         return True
 
 
     def play(self, game_state: dict) -> dict:
-        return
+        return {
+            "card_played": self._info["name"],
+            "mana_used": self._info["cost"],
+            "effect": self._info["effect"]
+        }
 
     def resolve_effect(self, targets: list) -> dict:
         return
