d569a89c5c359f45f9201d660360f5d4c549a91d
Joel Santos Souza <joesanto@c2r1s10.42porto.com>
2025-12-26 11:19:14 +0000

    little fixes

--- module06/alchemy/grimoire/spellbook.py
+++ module06/alchemy/grimoire/spellbook.py
@@ -1,6 +1,6 @@
 def record_spell(spell_name: str, ingredients: str) -> str:
     from . import validate_ingredients
     validation: str = validate_ingredients(ingredients)
-    if validation[8:] == " - VALID":
+    if validation[-8:] == " - VALID":
         return f"Spell recorded: {spell_name} ({validation})"
     return f"Spell rejected: {spell_name} ({validation})"
