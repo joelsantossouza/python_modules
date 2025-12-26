2e09cec41a74f7e2028eb565cf423f0833c425f7
Joel Santos Souza <joesanto@c2r1s10.42porto.com>
2025-12-26 10:49:25 +0000

    little fixes

--- module07/ex3/AggressiveStrategy.py
+++ module07/ex3/AggressiveStrategy.py
@@ -1,4 +1,8 @@
 from ex3.GameStrategy import GameStrategy
+from ex0.Card import Card
+from ex0.CreatureCard import CreatureCard
+from ex2.EliteCard import EliteCard
+
 
 class AggressiveStrategy(GameStrategy):
     """
@@ -7,11 +11,26 @@ class AggressiveStrategy(GameStrategy):
     """
 
     def execute_turn(self, hand: list, battlefield: list) -> dict:
-        return
+        if not isinstance(hand, list):
+            return None
+        hand = list[Card] = [
+            card for card in hand if isinstance(card, Card)
+        ]
+        sorted(hand, key=lambda card: card._info["cost"])
+        best_targets: list[Card] = self.prioritize_targets(battlefield)
+        return {
+            "best_cards": hand,
+            "best_targets": best_targets
+        }
+
+    def get_strategy_name(self) -> str:
+        return "AggressiveStrategy"
 
     def prioritize_targets(self, available_targets: list) -> list:
-        if not isinstance(available_target, list) not available_target:
+        if not isinstance(available_targets, list) or not available_targets:
             return None
-        one_hit_kill: list[CreatureCard] = []
-        more_hits_kill: list[CreatureCard] = []
-        for target in available_targets:
+        targets: list[Card] = [
+            target for target in available_targets if isinstance(target, (CreatureCard, EliteCard))
+        ]
+        sorted(targets, key=lambda card: card._info["health"])
+        return targets
