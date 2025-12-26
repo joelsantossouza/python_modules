b79e462dc8185aa23547ecdd7e63f40c034370fa
Joel Santos Souza <joesanto@c2r1s10.42porto.com>
2025-12-26 11:35:16 +0000

    little fixes

--- module06/alchemy/grimoire/validator.py
+++ module06/alchemy/grimoire/validator.py
@@ -2,8 +2,8 @@ def validate_ingredients(ingredients: str) -> str:
     if not isinstance(ingredients, str):
         return f"{ingredients} - INVALID"
     elements: list[str] = ["fire", "water", "earth", "air"]
-    ingredients = ingredients.split()
-    for ingredient in ingredients:
+    each_ingredient = ingredients.split()
+    for ingredient in each_ingredient:
         if ingredient not in elements:
             return f"{ingredients} - INVALID"
     return f"{ingredients} - VALID"
