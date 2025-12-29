b516989bc91c5b3cac4c635b48eb6372f11deee0
Joel Santos Souza <joesanto@c2r1s10.42porto.com>
2025-12-29 09:37:26 +0000

    little fix

--- module08/ex2/oracle.py
+++ module08/ex2/oracle.py
@@ -20,11 +20,17 @@ if __name__ == "__main__":
             file=stderr
         )
         exit(1)
-    vars: list[str] = [
-        getenv(var) for var in [
-            "DATABASE_URL", "API_KEY", "LOG_LEVEL", "ZION_ENDPOINT"
-        ]
+    env_vars: list[str] = [
+        "DATABASE_URL", "API_KEY", "LOG_LEVEL", "ZION_ENDPOINT"
     ]
+    vars: list[str] = [
+        getenv(var) for var in env_vars
+    ]
+    for i, var in enumerate(vars):
+        if not var:
+            print(f"Missing configuration of {env_vars[i]}", file=stderr)
+            exit(1)
+
     if mode == "production":
         info: list[str] = [
             "Database is connected", "Authenticated",
